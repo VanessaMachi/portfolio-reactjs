d6bf09e8a1beb45d58dc50c3437fe1e48d2841c3
Ee Lip Kang <happy5337@live.com>
2020-02-05 09:24:49 +0800

    Updates

--- precache-manifest.a3d7e2a74f6f89a616a14b46828221c6.js
+++ precache-manifest.a3d7e2a74f6f89a616a14b46828221c6.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5ba3db5ad9e270cde608b8b5dda2d53b",
+    "revision": "3038ea4d7bbae8bebfbe01bdc425593f",
     "url": "/portfolio-reactjs/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio-reactjs/static/css/2.559fd73f.chunk.css"
   },
   {
-    "revision": "b6d98998e3f93c707c8f",
+    "revision": "3ad31d8e41b48a53de55",
     "url": "/portfolio-reactjs/static/css/main.4a015291.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio-reactjs/static/js/2.bfad46f3.chunk.js.LICENSE"
   },
   {
-    "revision": "b6d98998e3f93c707c8f",
-    "url": "/portfolio-reactjs/static/js/main.7a7d3e8a.chunk.js"
+    "revision": "3ad31d8e41b48a53de55",
+    "url": "/portfolio-reactjs/static/js/main.82f2d2c1.chunk.js"
   },
   {
     "revision": "e8af35d6c50ff20f13c1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-reactjs/precache-manifest.a73792df22269d55b9732d20976a6115.js"
+  "/portfolio-reactjs/precache-manifest.a3d7e2a74f6f89a616a14b46828221c6.js"
 );
 
 self.addEventListener('message', (event) => {
