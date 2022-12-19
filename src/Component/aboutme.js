1c8ae9fa36649a2c8f37fe273080ef92d8eccfca
Lip Kang <callmekang1996@gmail.com>
2022-12-19 13:32:39 +0800

    refine portfolio

--- src/Component/aboutme.js
+++ src/Component/aboutme.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class AboutMe extends Component {
-    state = {  }
-    render() { 
-        return (
-            <div>
-                <h1>About Me</h1>
-            </div>
-        );
-    }
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>About Me</h1>
+      </div>
+    );
+  }
 }
- 
+
 export default AboutMe;
--- src/Component/resume.js
+++ src/Component/resume.js
@@ -1,85 +1,75 @@
-import React, { Component } from 'react';
-import { Grid, Cell} from 'react-mdl';
-import Education from './education';
-import Experience from './experience';
-import Skills from './skills';
+import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
+import Education from "./education";
+import Experience from "./experience";
+import Skills from "./skills";
 
 class Resume extends Component {
-    state = {  }
-    render() { 
-        return (
-            <div>
-                <Grid>
-                    <Cell col={4}>
-                        <div style={{textAlign:'center'}}>
-                            <img 
-                                src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
-                                alt="avatar"
-                                style={{height: "200px"}}
-                            />
-                        </div>
-                        <h2 style={{paddingTop: '2em'}}>Lip Kang</h2>
-                        <h4 style={{color: 'grey'}}>Software Engineer</h4>
-                        <hr style={{borderTop:'3px solid #833fb2', width: '50%'}}/>
-                        <p>
-                        Young, would like to create something great and desire to learn new things. 
-                        Also a tech enthusiast and would like to try approach new technology, 
-                        not a person for the sake of doing, bored task will kill me =D. 
-                        </p>
-                        <hr style={{borderTop:'3px solid #833fb2', width: '50%'}}/>
-                        {/* <h5>Address</h5>
+  state = {};
+  render() {
+    return (
+      <div>
+        <Grid>
+          <Cell col={4}>
+            <div style={{ textAlign: "center" }}>
+              <img
+                src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+                alt="avatar"
+                style={{ height: "200px" }}
+              />
+            </div>
+            <h2 style={{ paddingTop: "2em" }}>Lip Kang</h2>
+            <h4 style={{ color: "grey" }}>Software Engineer</h4>
+            <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
+
+            <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
+            {/* <h5>Address</h5>
                         <p>
                            
                         </p> */}
-                        <h5>Phone</h5>
-                        <p>010-2508029</p>
-                        <h5>Email</h5>
-                        <p>callmekang1996@gmail.com</p>
-                        <h5>Web</h5>
-                        <p>pending</p>
-                        <hr style={{borderTop:'3px solid #833fb2', width: '50%'}}/>
+            <h5>Phone</h5>
+            <p>010-2508029</p>
+            <h5>Email</h5>
+            <p>callmekang1996@gmail.com</p>
+            <h5>Web</h5>
+            <p>pending</p>
+            <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
+          </Cell>
 
-                    </Cell>
+          <Cell className="resume-right-col" col={8}>
+            <h2>Education</h2>
+            <Education
+              startYear={2016}
+              endYear={2018}
+              schoolName="INTI International University"
+              archievement="Degree of Computer Science"
+              schoolDescription="I am description"
+            />
+            <Education
+              startYear={2014}
+              endYear={2016}
+              schoolName="INTI International University"
+              archievement="Diploma of Information Technology"
+              schoolDescription="I am description"
+            />
+            <hr style={{ borderTop: "3px solid #e22947" }}></hr>
+            <h2>Experience</h2>
+            <Experience
+              startYear={2018}
+              endYear={2019}
+              companyName="M360 Ideology Sdn Bhd"
+              jobName="Programmer"
+              jobDescription="descriptio"
+            />
+            <hr style={{ borderTop: "3px solid #e22947" }}></hr>
 
-                    <Cell className="resume-right-col" col={8}>
-                        <h2>Education</h2>
-                        <Education
-                            startYear={2016}
-                            endYear={2018}
-                            schoolName ="INTI International University"
-                            archievement = "Degree of Computer Science"
-                            schoolDescription ="I am description"
-                        />
-                        <Education
-                            startYear={2014}
-                            endYear={2016}
-                            schoolName ="INTI International University"
-                            archievement = "Diploma of Information Technology"
-                            schoolDescription ="I am description"
-
-                        />
-                        <hr style={{borderTop: '3px solid #e22947'}}></hr>
-                        <h2>Experience</h2>
-                        <Experience 
-                            startYear={2018}
-                            endYear={2019}
-                            companyName="M360 Ideology Sdn Bhd"
-                            jobName="Programmer"
-                            jobDescription="descriptio"
-                        />
-                        <hr style={{borderTop: '3px solid #e22947'}}></hr>
-                        
-                        <h2>Skills</h2>
-                        <Skills
-                            skill="javascript"
-                            progress={10}
-                        />
-       
-                    </Cell>
-                </Grid>
-            </div>
-        );
-    }
+            <h2>Skills</h2>
+            <Skills skill="javascript" progress={10} />
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
 }
- 
+
 export default Resume;
